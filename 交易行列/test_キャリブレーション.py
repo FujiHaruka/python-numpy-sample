1e3cada72856e2e21dd9ec9287222a2b4204fb58
FujiHaruka <fizzyjohn1427@gmail.com>
2021-09-21 17:19:54 +0900

    キャリブレーション修正

--- 交易行列/test_キャリブレーション.py
+++ 交易行列/test_キャリブレーション.py
@@ -1,6 +1,6 @@
 import unittest
 import numpy as np
-from キャリブレーション import キャリブレート
+from キャリブレーション import キャリブレート, 行の和
 
 
 class TestStringMethods(unittest.TestCase):
@@ -17,10 +17,12 @@ class TestStringMethods(unittest.TestCase):
         # キャリブレーション結果を検証
         np.testing.assert_array_almost_equal(キャリブレーション結果, np.array([
             [0.333333, 0.0, 0.666667, 0.0],
-            [0.0, 0.428571, 0.0, 0.571429],
-            [0.639035, 0.0, 0.505263, 0.0],
-            [0.0, 0.571429, 0.0, 0.512195],
+            [0.0, 0.42857143, 0.0, 0.57142857],
+            [0.58024691, 0.0, 0.41975309, 0.0],
+            [0.0, 0.57142857, 0.0, 0.42857143],
         ]))
+        for 行 in range(4):
+            self.assertAlmostEqual(行の和(キャリブレーション結果, 行), 1.0)
 
 
 if __name__ == '__main__':

8476e8f986a6c3038867d289224e19f8e19e8687
FujiHaruka <fizzyjohn1427@gmail.com>
2021-10-02 21:52:09 +0900

    行方向の分配

--- 交易行列/test_キャリブレーションv3.py
+++ 交易行列/test_キャリブレーションv3.py
@@ -1,6 +1,6 @@
 import unittest
 import numpy as np
-from キャリブレーションv3 import 対角小行列の成分かどうかを調べる行列, 元行列から対角小行列を除外する
+from キャリブレーションv3 import 対角小行列の成分かどうかを調べる行列, 対角小行列を除外する, 移出ベクトルとの差を行方向に分配する
 
 
 class TestStringMethods(unittest.TestCase):
@@ -18,7 +18,7 @@ class TestStringMethods(unittest.TestCase):
             ])
         )
 
-    def test_元行列から対角小行列を除外する(self):
+    def test_対角小行列を除外する(self):
         元行列 = np.array([
             [1, 1, 1, 1, 1, 1],
             [1, 1, 1, 1, 1, 1],
@@ -29,7 +29,7 @@ class TestStringMethods(unittest.TestCase):
         ], dtype=float)
         対角小行列の成分か = 対角小行列の成分かどうかを調べる行列(元行列サイズ=6, 小行列サイズ=2)
         np.testing.assert_equal(
-            元行列から対角小行列を除外する(元行列, 対角小行列の成分か),
+            対角小行列を除外する(元行列, 対角小行列の成分か),
             np.array([
                 [0, 0, 1, 1, 1, 1],
                 [0, 0, 1, 1, 1, 1],
@@ -38,6 +38,31 @@ class TestStringMethods(unittest.TestCase):
                 [1, 1, 1, 1, 0, 0],
                 [1, 1, 1, 1, 0, 0],
             ], dtype=float)
+        )
+
+    def test_移出ベクトルとの差を行方向に分配する(self):
+        元行列 = np.array([
+            [0, 0, 1, 2, 1, 2],
+            [0, 0, 3, 4, 3, 1],
+            [1, 2, 0, 0, 1, 1],
+            [4, 4, 0, 0, 3, 4],
+            [1, 2, 5, 2, 0, 0],
+            [3, 4, 2, 4, 0, 0],
+        ], dtype=float)
+        移出ベクトル = np.array([10, 10, 10, 10, 10, 10], dtype=float)
+        Bベクトル = np.array([6, 11, 5, 15, 10, 13], dtype=float)
+        対角小行列の成分か = 対角小行列の成分かどうかを調べる行列(元行列サイズ=6, 小行列サイズ=2)
+        行方向の補正量分配行列 = 移出ベクトルとの差を行方向に分配する(元行列, 移出ベクトル, Bベクトル, 対角小行列の成分か)
+        np.testing.assert_almost_equal(
+            行方向の補正量分配行列,
+            np.array([
+                [0, 0, 0.66666667, 1.33333333, 0.66666667, 1.33333333],
+                [0, 0, 0, 0, 0, 0],
+                [1, 2, 0, 0, 1, 1],
+                [0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0]
+            ])
         )
 
 
